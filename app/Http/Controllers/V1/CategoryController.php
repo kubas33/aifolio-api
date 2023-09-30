38539d9ae1eb56408af15f89e8d652c0831dec0f
kubas33 <Stasiak91@gmail.com>
2023-09-30 16:05:31 +0200

    Add controller, resource and services for category

--- app/Http/Controllers/V1/CategoryController.php
+++ app/Http/Controllers/V1/CategoryController.php
@@ -1,34 +1,60 @@
 <?php
 
 namespace App\Http\Controllers\V1;
+
+use App\Exceptions\JsonResponseException;
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\CategoryResource;
 use App\Models\Category;
+use App\Services\V1\CategoryQueryService;
+use App\Services\V1\CategoryService;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends Controller
 {
+    protected CategoryService $categoryService;
+    protected CategoryQueryService $categoryQueryService;
+
+    public function __construct(CategoryService $categoryService, CategoryQueryService $categoryQueryService) {
+        $this->categoryService = $categoryService;
+        $this->categoryQueryService = $categoryQueryService;
+    }
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        $data = $this->categoryQueryService->query();
+
+        return ResponseHelper::pageResponse($data, CategoryResource::class, Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-        //
+        $data = $request->validate([
+            'name'=> 'required|min:3|unique:categories,name',
+        ]);
+
+        $category = $this->categoryService->create($data);
+        return ResponseHelper::response(new CategoryResource($category), Response::HTTP_OK);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Category $category)
+    public function show(Category $category): JsonResponse
     {
-        //
+        return ResponseHelper::response(new CategoryResource($category), Response::HTTP_OK);
     }
 
 
@@ -37,14 +63,22 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+        $data = $request->validate([
+            'name'=> ['required','min:3', Rule::unique('categories', 'name')->ignore($category)],
+        ]);
+        $category = $this->categoryService->update($category, $data);
+
+        return ResponseHelper::response(new CategoryResource($category), Response::HTTP_OK);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Category $category)
+    public function destroy(Category $category): JsonResponse
     {
-        //
+
+        $category = $this->categoryService->delete($category);
+        return ResponseHelper::response(new CategoryResource($category), Response::HTTP_OK);
+
     }
 }
---
+++ app/Services/V1/CategoryService.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Services\V1;
+
+use App\Exceptions\JsonResponseException;
+use App\Models\Category;
+use Exception;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class CategoryService
+{
+    /**
+     * Create a new category.
+     *
+     * @param  array $data
+     * @return Category
+     */
+    public function create(array $data): Category
+    {
+        try {
+            $category = new Category();
+            $category->name = $data['name'];
+            $category->slug = Str::slug($data['name']);
+            $res = $category->save();
+
+            return $res ? $category : throw new Exception('CANT_STORE_CATEGORY');
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
+            throw new JsonResponseException('Cant store category', Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Update a category.
+     *
+     * @param  Category $category
+     * @param  array $data
+     * @return Category
+     */
+    public function update(Category $category, array $data): Category
+    {
+        try {
+            $category->name = $data['name'];
+            $category->slug = Str::slug($data['name']);
+            $res = $category->save();
+
+            return $res ? $category : throw new Exception('CANT_UPDATE_CATEGORY');
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
+            throw new JsonResponseException('Can\'t update category', Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Delete a category.
+     *
+     * @param  Category $category
+     * @return Category
+     */
+    public function delete(Category $category): Category
+    {
+        try {
+            $res = $category->delete();
+
+            return $res ? $category : throw new Exception('CANT_DELETE_CATEGORY');
+
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
+            throw new JsonResponseException('Can\'t delete category', Response::HTTP_BAD_REQUEST);
+        }
+    }
+}
