14459e9faca0302e271e67136333f21b88643f33
kubas33 <Stasiak91@gmail.com>
2023-09-30 17:23:16 +0200

    Add phpDocs for ResponseHelper

--- app/Helpers/ResponseHelper.php
+++ app/Helpers/ResponseHelper.php
@@ -6,11 +6,28 @@ use Illuminate\Http\JsonResponse;
 
 abstract class ResponseHelper
 {
-    public static function response($data, $status): JsonResponse {
+    /**
+     * Generate a JSON response.
+     *
+     * @param mixed $data
+     * @param int $status
+     * @return JsonResponse
+     */
+    public static function response($data, $status): JsonResponse
+    {
         return response()->json($data, $status);
     }
 
-    public static function pageResponse($data, $resourceClass, $status): JsonResponse {
+    /**
+     * Generate a JSON response for paginated data.
+     *
+     * @param mixed $data
+     * @param mixed $resourceClass
+     * @param int $status
+     * @return JsonResponse
+     */
+    public static function pageResponse($data, $resourceClass, $status): JsonResponse
+    {
         return self::response([
             'data' => $resourceClass::collection($data->items()),
             'total' => $data->total(),
