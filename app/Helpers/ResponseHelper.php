af85fbf1ae3601bc09215ec3a5a6642d4418cac1
kubas33 <Stasiak91@gmail.com>
2023-09-30 16:03:59 +0200

    Add response helper

--- app/Helpers/ResponseHelper.php
+++ app/Helpers/ResponseHelper.php
@@ -9,4 +9,14 @@ abstract class ResponseHelper
     public static function response($data, $status): JsonResponse {
         return response()->json($data, $status);
     }
+
+    public static function pageResponse($data, $resourceClass, $status): JsonResponse {
+        return self::response([
+            'data' => $resourceClass::collection($data->items()),
+            'total' => $data->total(),
+            'page' => $data->currentPage(),
+            'lastPage' => $data->lastPage(),
+            'perPage' => $data->perPage(),
+        ], $status);
+    }
 }
