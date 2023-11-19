938d61a6a87193c4f14ee20c7e02a5785839d36a
kubas33 <44829224+kubas33@users.noreply.github.com>
2023-11-19 21:02:01 +0100

    Usunięcie niepotrzebnych metod z observera

--- app/Observers/AiImageObserver.php
+++ app/Observers/AiImageObserver.php
@@ -6,21 +6,6 @@ use App\Models\AiImage;
 
 class AiImageObserver
 {
-    /**
-     * Handle the AiImage "created" event.
-     */
-    public function created(AiImage $aiImage): void
-    {
-        //TODO Czy dodać tutaj AiImageFilenamService->create  (zamiast wywoływania w AiImageService) ??
-    }
-
-    /**
-     * Handle the AiImage "updated" event.
-     */
-    public function updated(AiImage $aiImage): void
-    {
-        //TODO Czy dodać tutaj AiImageFilenamService->update (zamiast wywoływania w AiImageService) ??
-    }
 
     /**
      * Handle the AiImage "deleted" event.
@@ -30,21 +15,5 @@ class AiImageObserver
         foreach ($aiImage->aiImageFilenames as $imageFilename) {
             $imageFilename->delete();
         }
-    }
-
-    /**
-     * Handle the AiImage "restored" event.
-     */
-    public function restored(AiImage $aiImage): void
-    {
-        //
-    }
-
-    /**
-     * Handle the AiImage "force deleted" event.
-     */
-    public function forceDeleted(AiImage $aiImage): void
-    {
-        //
     }
 }
